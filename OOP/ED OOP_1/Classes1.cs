eede2fc71ceebd1132a86f3997b203d14af92840
Hasan <hasanktu98@outlook.com>
2022-11-26 15:35:20 +0300

    First  c# commit | Engin Demiroğ Lesson OOP

---
+++ OOP/ED OOP_1/Classes1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engin_Demirog_OOP_1_11._11._2022
+{
+    public class Classes1
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Console.ReadLine();
+        }    
+
+    
+        class CustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer Added ");
+            }
+
+            public void Delete()
+            {
+                Console.WriteLine("Customer Deleted");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Customer İnformation Updated");
+            }
+        }
+    }
+}
